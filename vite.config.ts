5b20d1da5105c5540d8990d1baa793debeba5ce1
Kikoman38 <kobeparks2019@gmail.com>
2025-09-30 14:10:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
-    base: './', // 👈 important for Render deployment
+    base: './', //
     server: {
       port: 3000,
       host: '0.0.0.0',
